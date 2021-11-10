f1271a5163d947be9bdac27bb6931610114b5bd6
Todesbengel0 <73591236+Todesbengel0@users.noreply.github.com>
2021-11-10 12:20:45 +0100

    Funktionsüberladungen bei Perlin Noise hinzugefügt

--- Aufgaben/Aufgaben/PerlinNoise.cpp
+++ Aufgaben/Aufgaben/PerlinNoise.cpp
@@ -1,8 +1,19 @@
 #include "pch.h"
 #include "PerlinNoise.h"
 
+/// Create a PerlinNoise with numOctaves and startValue = 1
+CPerlinNoise::CPerlinNoise(int numOctaves)
+	: m_numOctaves(numOctaves), m_startValue(1)
+{
+	m_Octaves = new COctave * [m_numOctaves];
+	for (int i = 0; i < m_numOctaves; ++i)
+	{
+		m_Octaves[i] = new COctave(i + 1);
+	}
+}
+
 CPerlinNoise::CPerlinNoise(int numOctaves, int startValue)
-	: m_numOctaves(numOctaves), m_startValue(startValue), m_persistance(0.5f)
+	: m_numOctaves(numOctaves), m_startValue(startValue)
 {
 	m_Octaves = new COctave*[m_numOctaves];
 	for (int i = 0; i < m_numOctaves; ++i)
@@ -18,6 +29,12 @@ CPerlinNoise::~CPerlinNoise()
 	delete[] m_Octaves;
 }
 
+/// <summary>
+/// Calculates the Perlin Noise at a specific time.
+/// </summary>
+/// <param name="time">Time of which to get the Signal value from</param>
+/// <param name="persistance">Value how much each further Ocatave should contribute more to the final value.</param>
+/// <returns></returns>
 float CPerlinNoise::GetValue(float time, float persistance)
 {
 	float output = 0;
@@ -26,4 +43,14 @@ float CPerlinNoise::GetValue(float time, float persistance)
 		output += m_Octaves[i]->GetSignal(time) * std::pow(persistance, m_startValue + i);
 	}
 	return output;
+}
+
+/// <summary>
+/// Calculates the Perlin Noise at a specific time with persistance = 0.5
+/// </summary>
+/// <param name="time">Time of which to get the Signal value from</param>
+/// <returns>Signal value at time</returns>
+float CPerlinNoise::GetValue(float time)
+{
+	return GetValue(time, 0.5);
 }
--- Aufgaben/Aufgaben/PerlinNoise.h
+++ Aufgaben/Aufgaben/PerlinNoise.h
@@ -4,15 +4,16 @@
 class CPerlinNoise
 {
 public:
+	CPerlinNoise(int numOctaves);
 	CPerlinNoise(int numOctaves, int startValue);
 	~CPerlinNoise();
 
 	float GetValue(float time, float persistance);
+	float GetValue(float time);
 
 
 private:
 	const int m_numOctaves;
 	const int m_startValue;
-	int m_persistance;
 	COctave** m_Octaves;
 };
