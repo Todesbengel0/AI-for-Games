6a67b82b63539065a182bd8d093d2e2c193f0358
Todesbengel0 <73591236+Todesbengel0@users.noreply.github.com>
2021-11-22 17:25:57 +0100

    WANDER prallt an Border ab
    WANDER an WorldBorder mit Einfallswinkel = Ausfallswinkel (funktioniert nicht)

--- Aufgaben/Aufgaben/SteeringBehaviorDynamicWANDER.cpp
+++ Aufgaben/Aufgaben/SteeringBehaviorDynamicWANDER.cpp
@@ -1,6 +1,9 @@
 #include "pch.h"
 #include "SteeringBehaviorDynamicWANDER.h"
 #include "Npc.h"
+#include "KnowledgeWorldBorder.h"
+
+#define EPSENV(x)	0.95f * (float)x
 
 CRandom CSteeringBehaviorDynamicWANDER::ms_Rnd;
 
@@ -20,7 +23,7 @@ CSteeringBehaviorDynamicWANDER::~CSteeringBehaviorDynamicWANDER()
 
 SSteeringForce CSteeringBehaviorDynamicWANDER::GetForce()
 {
-	SSteeringForce res;
+	SSteeringForce resForce;
 
 	// 
 	CHVector vCircleCenter = m_pUser->GetKinematics().GetOrientationVec() * m_fOffset;
@@ -34,7 +37,24 @@ SSteeringForce CSteeringBehaviorDynamicWANDER::GetForce()
 	CHVector vTargetDir = vCircleCenter + vAngleDir * m_fRadius;
 	vTargetDir.Norm();
 
-	res.vMovementForce = vTargetDir * m_pUser->GetKinematics().GetMaxMovementForce();
+	// NPC has Borderline-Syndrom
+	if (std::abs(m_pUser->GetKinematics().GetPosition().x) >= EPSENV(m_pWorldBorder->GetBoardSize().x))
+	{
+		CHVector vXNorm = CHVector(1.0f, 0.0f, 0.0f);
+		resForce.vMovementForce -= vXNorm * (resForce.vMovementForce * vXNorm) * 2;
+	}
+	if (std::abs(m_pUser->GetKinematics().GetPosition().z) >= EPSENV(m_pWorldBorder->GetBoardSize().z))
+	{
+		CHVector vZNorm = CHVector(0.0f, 0.0f, 1.0f);
+		resForce.vMovementForce -= vZNorm * (resForce.vMovementForce * vZNorm) * 2;
+	}
 
-	return res;
+	resForce.vMovementForce = vTargetDir * m_pUser->GetKinematics().GetMaxMovementForce();
+
+	// für korrekten Winkel muss Z-Achse invertiert werden (Brummkreisel!!)
+	/*CHVector vFixedDir = resForce.vMovementForce;
+	vFixedDir.z = -vFixedDir.z;
+	resForce.fRotationForce = vFixedDir.AngleXZ();*/
+
+	return resForce;
 }
