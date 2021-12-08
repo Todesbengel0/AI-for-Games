6556f356624ac17b01b4583843fc882053134574
Todesbengel0 <73591236+Todesbengel0@users.noreply.github.com>
2021-12-08 14:28:00 +0100

    GetCharactersNear and InFOV

--- Aufgaben/Aufgaben/KnowledgeWorldCharacters.h
+++ Aufgaben/Aufgaben/KnowledgeWorldCharacters.h
@@ -13,6 +13,10 @@ public:
 	~CKnowledgeWorldCharacters();
 
 	const character_vec_t& GetCharacters() const;
+	const character_vec_t GetCharactersNear(CHVector position, float maxDistance = 10.0f);
+	const character_vec_t GetCharactersNear(CCharacter* source, float maxDistance = 10.0f);
+	const character_vec_t GetCharactersInFOV(CHVector position, CHVector viewDir, float maxDiffAngle = QUARTERPI, float maxDistance = 25.0f);
+	const character_vec_t GetCharactersInFOV(CCharacter* source, float maxDiffAngle = QUARTERPI, float maxDistance = 25.0f);
 
 private:
 	character_vec_t m_Characters;
--- Aufgaben/Aufgaben/SteeringBehaviorVELOCITY_MATCHING.cpp
+++ Aufgaben/Aufgaben/SteeringBehaviorVELOCITY_MATCHING.cpp
@@ -27,51 +27,33 @@ SSteeringForce CSteeringBehaviorVELOCITY_MATCHING::GetForce(float fTimeDelta)
 		return resForce;
 	}
 
-	const CKnowledgeWorldCharacters::character_vec_t& buddies = m_Buddies->GetCharacters();
-	if (buddies.size() <= 1)
+	// Neue Variante
+	const CKnowledgeWorldCharacters::character_vec_t buddies = m_Buddies->GetCharactersNear(m_pUser, m_fActivationDistance);
+	if (buddies.size() == 0)
 	{
 		resForce.vMovementForce = vPreviousMovementForce;
 		resForce.fRotationForce = AngleVektoriaToZX(resForce.vMovementForce);
 		return resForce;
 	}
 
+	// AverageVelocity ist der Vektor, mit dem der NPC sich gerne bewegen würde, um der Gruppe zu folgen
 	CHVector vAverageVelocity(0.0f, 0.0f, 0.0f, 0.0f); // muss W=0 initialisiert werden!
-	int buddyCount = 0;
 	float fAccumulatedSpeed = 0.0f;
 	for (auto buddy : buddies)
 	{
-		if (buddy == m_pUser)
-			continue;
-
-		// NPC zum Ziel
-		CHVector vToTarget = buddy->GetKinematics().GetPosition() - m_pUser->GetKinematics().GetPosition();
-		float fDist = vToTarget.Length();
-
-		if (fDist < m_fActivationDistance)
-		{
-			vAverageVelocity += buddy->GetKinematics().GetMovementForce();
-			fAccumulatedSpeed += buddy->GetKinematics().GetMovementForce().Length();
-			++buddyCount;
-		}
+		vAverageVelocity += buddy->GetKinematics().GetMovementForce();
+		fAccumulatedSpeed += buddy->GetKinematics().GetMovementForce().Length();
 	}
-
-	// Wenn keiner in der Nähe: Weiterlaufen wie zuvor (Vielleicht kann man auch Wander einfügen?) !!Macht irgendwie Probleme am Rand des Feldes
-	if (buddyCount == 0)
-	{
-		resForce.vMovementForce = vPreviousMovementForce;
-		resForce.fRotationForce = AngleVektoriaToZX(resForce.vMovementForce);
-		return resForce;
-	}
-
 	// Mitteln der Bewegungen (wenn Summe der Bewegungen nicht Nullvektor)
-	if (buddyCount > 1 && vAverageVelocity.Length() > 0.01f)
-		ScaleVectorTo(vAverageVelocity, fAccumulatedSpeed / (float)buddyCount);
+	if (buddies.size() > 1 && vAverageVelocity.Length() > 0.01f)
+		ScaleVectorTo(vAverageVelocity, fAccumulatedSpeed / (float)buddies.size());
 
 	resForce.vMovementForce = vAverageVelocity + vPreviousMovementForce;
 	if (resForce.vMovementForce.Length() < 0.01f)	// passiert eig. nur, wenn NPCs re-spawned werden
 		return resForce;
 
-	ScaleVectorTo(resForce.vMovementForce, m_pUser->GetKinematics().GetMaxMovementForce());
+	// ScaleVectorTo(resForce.vMovementForce, m_pUser->GetKinematics().GetMaxMovementForce());
+	ScaleVectorTo(resForce.vMovementForce, (vAverageVelocity.Length() + vPreviousMovementForce.Length()) * 0.5f);
 
 	// Überprüfung, ob es zu einer Beschleunigung kommt -> Anwenden der Beschleunigungsregulationen
 
